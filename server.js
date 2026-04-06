517b32fee602f32b56b9e2e45a5460bb8d334c79
mohitdarji0213 <mohitdarji0213@gmail.com>
2026-04-06 14:54:28 +0530

    Update CORS middleware to allow multiple origins

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: [
+    'https://shyam-photo-gallery-frontend.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}))
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
